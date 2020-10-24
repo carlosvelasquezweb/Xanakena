9860ec2e1dde1b8b5b3793ed0ed880c7c5fc4069
Carlos Alfredo Velásquez Ferrer <ca27693@gmail.com>
2020-10-24 14:48:34 -0300

    Componente ItemListContainer y CartIcon

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import CartIcon from './components/CartIcon';
 
 function App() {
 
   return <>
     <NavBar />
-    <ItemListContainer name="Productos Xanakena"/>
+    <ItemListContainer name="Productos Xanakena"><CartIcon/>
+    </ItemListContainer>
     
   </>
 }
---
+++ src/components/CartIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CartIcon(){
+    return <a><i className="fi fi-shopping-basket"></i></a>
+}
+
+export default CartIcon;
