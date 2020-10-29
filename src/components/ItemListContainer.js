7694f216937e74f081f1bf4a2d2d412701d98c84
Carlos Alfredo Velásquez Ferrer <ca27693@gmail.com>
2020-10-28 21:46:41 -0300

    Itemcount 7.0

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,7 +4,7 @@ import ItemCount from './ItemCount';
 
 function ItemListContainer(props) {
 
-   const onAdd=(props)=>{if(props<= 0){alert("No hay items seleccionados!")}else{alert("Items en el carrito: "+props)}};
+   function onAdd(cantidad) { if (cantidad <= 0) { alert("No hay items seleccionados!"); } else { alert("Items en el carrito: " + cantidad); } }
 
     return <div className="container">
         <div className="row">
@@ -12,10 +12,10 @@ function ItemListContainer(props) {
             <h2 className="text-center">{props.name}</h2>
         </div><hr />
         <div className="row">
-            <p>Producto #1</p><ItemCount stock="10" initial="0" onAdd={onAdd}/>
-            <p>Producto #2</p><ItemCount stock="10" initial="0" onAdd={onAdd}/>
-            <p>Producto #3</p><ItemCount stock="10" initial="0" onAdd={onAdd}/>
-            <p>Producto #4</p><ItemCount stock="10" initial="0" onAdd={onAdd}/>
+            <p>Producto #1</p>  <ItemCount stock="10" initial="0" onAdd={onAdd} />
+            <p>Producto #2 </p> <ItemCount stock="10" initial="0" onAdd={onAdd}/>
+            <p>Producto #3 </p> <ItemCount stock="10" initial="0" onAdd={onAdd}/>
+            <p>Producto #4  </p><ItemCount stock="10" initial="0" onAdd={onAdd}/>
         </div>
     </div>
 
